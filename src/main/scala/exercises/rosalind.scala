76d9976b914fbf2e7befbb8dcec3518f88e4e3ae
Eduardo Santamaria <edsanav@gmail.com>
2021-07-03 13:00:41 +0200

    Extending tests

--- src/main/scala/exercises/rosalind.scala
+++ src/main/scala/exercises/rosalind.scala
@@ -9,7 +9,7 @@ object rosalind {
 
   def run(args:List[String]):IO[ExitCode] = {
     val result = args match {
-      case "ex1"::input::_ => exercise1(input).map(_.show)
+      case "ex1"::input::_ => ntComposition(input).map(_.show)
       case _ => Left("Invalid input")
     }
     result match  {
@@ -20,22 +20,36 @@ object rosalind {
   }
 
   case class Composition(A:Int, C:Int, G:Int, T:Int) {
-    def add(nuc:Char):Either[String, Composition] = nuc match {
-      case 'A' => Right(Composition(A+1, C, G, T))
-      case 'C'=>Right(Composition(A, C+1, G, T))
-      case 'G' => Right(Composition(A, C, G+1, T))
-      case 'T'=> Right(Composition(A, C, G, T+1))
-      case _ => Left(s"Invalid nucleotide $nuc")
+    def +(c: Composition):Composition = Composition(A+c.A, C+c.C, G+c.G, T+c.T)
+  }
+
+  object Composition {
+    def parse(nt:Char):Either[String, Composition] = nt match {
+      case 'A' => Right(Composition(1, 0, 0, 0))
+      case 'C'=>Right(Composition(0, 1, 0, 0))
+      case 'G' => Right(Composition(0, 0, 1, 0))
+      case 'T'=> Right(Composition(0,0,0,1))
+      case _ => Left(s"Invalid nucleotide $nt\n")
     }
   }
 
   implicit val compShow: Show[Composition] = (t: Composition) => s"A: ${t.A} C: ${t.C}, G: ${t.G}, T:${t.T}"
 
-  def exercise1(input:String):Either[String, Composition] = {
+  def ntComposition(input:String):Either[String, Composition] = {
     input.foldLeft(Composition(0,0,0,0).asRight[String]){
-      case (Left(x), _) => Left(x)
-      case (Right(z), nuc) => z.add(nuc)
+      case (Left(x), _) => Left(x) // only one error is carried
+      case (Right(z), nt) => Composition.parse(nt) match {
+        case Right(c) => Right(z+c)
+        case x => x
+      }
     }
   }
+
+//  def ntCompositionErrors(input:String):Either[String, Composition] = {
+//    input.foldLeft(Composition(0,0,0,0).asRight[String]){
+//      case (Left(x), _) => Left(x) // only one error is carried
+//      case (Right(z), nt) => Right(z + Composition.parse(nt))
+//    }
+//  }
 
 }
--- src/test/scala/exercises/RosalindSuite.scala
+++ src/test/scala/exercises/RosalindSuite.scala
@@ -1,48 +1,89 @@
 package exercises
 
-import exercises.rosalind.{Composition, exercise1}
+import exercises.rosalind.{Composition, ntComposition}
+import org.scalacheck.Prop.{forAll, forAllNoShrink}
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.Checkers
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
 
-class RosalindSuite extends AnyFunSuite with ScalaCheckDrivenPropertyChecks with Checkers {
+class RosalindSuite extends AnyFunSuite  with Checkers {
 
   val nucleotideGen:Gen[Char] = Gen.oneOf('A', 'C', 'G', 'T')
   val nonNucleotide:Gen[Char] = Arbitrary.arbitrary[Char].filterNot( c => c =='A' | c=='C' | c=='G' | c=='T')
   val DNAGen:Gen[String] = Gen.stringOf(nucleotideGen)
-  val DNAGenWithNotDNA:Gen[String] = DNAGen.map(s => s + nonNucleotide)
-  val noDNA:Gen[String] = Gen.stringOf(nonNucleotide)
+  val nonDNA:Gen[String] = nonEmptyStringOf(Arbitrary.arbitrary[Char])
 
-  def definedFreq(A:Int, C:Int, G: Int, T:Int):Gen[String] = Gen.stringOfN(1000, Gen.frequency(
-    (A, Gen.const('A')),(C,Gen.const('C')), (G, Gen.const('G')), (T, Gen.const('T'))
-  ))
-
+  def nonEmptyStringOf(g:Gen[Char]):Gen[String] = Gen.nonEmptyListOf[Char](g).map(_.mkString)
 
   test("Invariance") {
     forAll(DNAGen) { dna:String =>
-      exercise1(dna).isRight
+      ntComposition(dna).isRight
     }
   }
 
   test("Invariance left") {
-    forAll(DNAGenWithNotDNA) { dna:String =>
-      exercise1(dna).isLeft
+    // Non empty string because an empty one would return a valid composition response
+    forAll(nonDNA){ nonDna:String =>
+      ntComposition(nonDna).isLeft
     }
   }
 
-  //https://github.com/typelevel/scalacheck/blob/main/doc/UserGuide.md
-  test("Expected frequencies"){
-    val x = 5
-    forAll(definedFreq(x, 2*x, 3*x, 4*x)){ (dna:String) =>
-      exercise1(dna) match {
-        case Right(c:Composition) =>println(c.C.toFloat / c.A); assert((c.C.toFloat / c.A) == 2.0)
-        case _ => fail("Not a valid output")
+  test("length must be equal to total sum"){
+    check(forAllNoShrink(DNAGen){ dna:String =>
+      ntComposition(dna) match {
+        case Right(c:Composition) => println(dna.length); dna.length == (c.A + c.C + c.G + c.T)
+        case _ => fail(s"not a valid output $dna")
       }
-    }
-
+    })
   }
+
+  test("only A"){
+    check(forAllNoShrink(nonEmptyStringOf(Gen.const('A'))){ dna:String =>
+      ntComposition(dna) match {
+        case Right(c:Composition) => c.A == dna.length && c.C == 0 && c.G == 0 && c.T == 0
+        case _ => fail(s"not a valid output $dna")
+      }
+    })
+  }
+
+  test("only C"){
+    check(forAllNoShrink(nonEmptyStringOf(Gen.const('C'))){ dna:String =>
+      ntComposition(dna) match {
+        case Right(c:Composition) => c.C == dna.length && c.A == 0 && c.G == 0 && c.T == 0
+        case _ => fail(s"not a valid output $dna")
+      }
+    })
+  }
+
+  test("only G"){
+    check(forAllNoShrink(nonEmptyStringOf(Gen.const('G'))){ dna:String =>
+      ntComposition(dna) match {
+        case Right(c:Composition) => c.G == dna.length && c.A == 0 && c.C == 0 && c.T == 0
+        case _ => fail(s"not a valid output $dna")
+      }
+    })
+  }
+
+  test("only T"){
+    check(forAllNoShrink(nonEmptyStringOf(Gen.const('T'))){ dna:String =>
+      ntComposition(dna) match {
+        case Right(c:Composition) => c.T == dna.length && c.A == 0 && c.C == 0 && c.G == 0
+        case _ => fail(s"not a valid output $dna")
+      }
+    })
+  }
+
+//  test("Expected A"){
+//    val x = 5
+//    forAll(definedFreq(x, x, x, x)){ (dna:String) =>
+//      ntComposition(dna) match {
+//        case Right(c:Composition) =>println(c.C.toFloat / c.A); assert((c.C.toFloat / c.A) == 2.0)
+//        case _ => fail("Not a valid output")
+//      }
+//    }
+//
+//  }
 
 
 }
