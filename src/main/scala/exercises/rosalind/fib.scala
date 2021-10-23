eaeb88681338bec7353a773ab9fb1b0f7fb8b2bb
Eduardo Santamaria <edsanav@gmail.com>
2021-10-23 20:58:58 +0200

    WIP

--- src/main/scala/exercises/rosalind/fib.scala
+++ src/main/scala/exercises/rosalind/fib.scala
@@ -9,13 +9,14 @@ import scala.util.Try
 object fib {
 
   def run(@unused input:List[String]):IO[ExitCode] = {
-    input match  {
-      case nS::kS::_ =>
-        IO.fromEither(parse(nS,kS)
-          .map{case (n,k) => compute(n,k)})
-          .map(println(_)) *> IO(ExitCode.Success)
-      case _ => IO(ExitCode.Error)
-    }
+    IO.println(compute3(5,3)) *> IO(ExitCode.Success)
+//    input match  {
+//      case nS::kS::_ =>
+//        IO.fromEither(parse(nS,kS)
+//          .map{case (n,k) => compute(n,k)})
+//          .map(println(_)) *> IO(ExitCode.Success)
+//      case _ => IO(ExitCode.Error)
+//    }
   }
 
   def parse(nStr:String, kStr:String):Either[Throwable, (Int,Int)] = {
@@ -27,11 +28,62 @@ object fib {
 
   def compute(N:Int, K:Int):Int = {
     @tailrec
-    def inner(month:Int, current:Int, nextGen:Int):Int = month match {
-      case N => current
-      case _ => {inner(month+1, nextGen, current*K+nextGen)}
+    def inner(month:Int, current:Int, nextGen:Int):Int = {
+      month match {
+        case N => current
+        case _ => {inner(month+1, nextGen, current*K+nextGen)}
+      }
     }
     inner(1, 1, 1)
   }
+
+  def compute2(N:Int, K:Int):Int = {
+    @tailrec
+    def inner(month:Int, adults:Int, children:Int):Int = {
+      month match {
+        case N => adults+children
+        case _ => inner(month+1, adults+children, adults*K)
+      }
+    }
+    inner(1, 0, 1)
+  }
+
+  def compute3(N:Int, K:Int):Int = {
+    def inner(month:Int, adults:Int, children:Int):Int = {
+      month match {
+        case N => adults+children
+        case _ => adults + inner(month+1, children, adults*K)
+      }
+    }
+    inner(1, 0, 1)
+  }
+
+  def computeDead(N:Int, M:Int):Int = {
+
+    @tailrec
+    def inner(totalMonth:Int, remainingMonths:Int, adults:Int, children:Int):Int = {
+      (totalMonth, remainingMonths) match {
+        case (_, 0) => 0
+        case (N, _) => adults+children
+        case _ => inner(totalMonth+1, remainingMonths-1, adults, children) + inner(month+1, children, adults*K)
+      }
+    }
+    inner(1, 0, 1)
+  }
+
+  /*
+  (1(3), 1(2)
+  (1, 1)
+  (1, 2)
+  (2, 3)
+  (3, 5)
+  (5, 8)
+
+  1,             1(2)
+  1(1),          1(0)+1(2))
+  1(0)+1(2),    1(0)+1(1)+1(2)
+  1(1)+1(2),
+
+   */
 
 }
